2e550fa292b4e461100136d17501da1a406a71b5
ingrid139 <ingrid139@hotmail.com>
2020-03-01 07:51:38 -0300

    add laço for extra

--- P12-LacoRepeticaoFor2/Program.cs
+++ P12-LacoRepeticaoFor2/Program.cs
@@ -6,21 +6,37 @@ namespace P12_LacoRepeticaoFor2
     {
         static void Main(string[] args)
         {
-            for (int numero = 0; numero <= 10; numero++)
+            double valorInvestido = 1000;
+            double fatorRendimento = 1.0036;
+
+            for (int contadorAno = 1; contadorAno <= 5; contadorAno++)
             {
-                Console.Write("Tabuada do " + numero + " : ");
-
-                for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+                for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
                 {
-                    Console.Write(numero * multiplicador);
-                    Console.Write(" ");
+                    valorInvestido *= fatorRendimento;
                 }
-
-                Console.WriteLine();
+                fatorRendimento += 0.0010;
             }
 
-
+            Console.WriteLine("Ao término do investimento, você terá R$" + valorInvestido);
             Console.ReadLine();
+        
+            //exemplo extra
+            //for (int numero = 0; numero <= 10; numero++)
+            //{
+            //    Console.Write("Tabuada do " + numero + " : ");
+
+            //    for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+            //    {
+            //        Console.Write(numero * multiplicador);
+            //        Console.Write(" ");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+
+        Console.ReadLine();
         }
     }
 }
