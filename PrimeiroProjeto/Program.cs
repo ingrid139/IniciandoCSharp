e00720e9306aabf0efd6f28d272c18b51347ad2a
ingrid139 <ingrid139@hotmail.com>
2020-10-20 19:19:43 -0300

    Add Algoritmos exemplos

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -8,6 +8,30 @@ namespace PrimeiroProjeto
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Projeto no Visual Studio!");
+            Console.WriteLine("Olá, eu sou a Ingrid!");
+
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("O nome digitado foi: " + nome);
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            //*********************************************************
+            Console.WriteLine("Informe sua idade");
+
+            string idadeDigitada = Console.ReadLine();
+            int idade = Int32.Parse(idadeDigitada);
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("É maior de idade");
+            }
+            else
+            {
+                Console.WriteLine("Não é maior de idade");
+
+            }
 
             Console.ReadKey();
         }
